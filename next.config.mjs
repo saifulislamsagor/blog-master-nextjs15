99d1c79dc53a421fa52405cf37ec68c08b61590f
saifulislamsagor <saifulislamsagor.gpg.bd@gmail.com>
2025-04-10 17:27:16 +0600

    dfghj

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "lh3.googleusercontent.com",
+        hostname: "lh3.gratisography.com",
         port: "",
       },
       {
