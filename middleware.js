eea34e80bef7a28418d231f780fa27ff8e0fc37b
saifulislamsagor <saifulislamsagor.gpg.bd@gmail.com>
2025-04-14 20:01:47 +0600

    updated publicpath

--- middleware.js
+++ middleware.js
@@ -1,10 +1,51 @@
+// import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+
+// export default withAuth(
+//   async function middleware() {
+//   },
+//   {
+//     // Middleware still runs on all routes, but doesn't protect the blog route
+//     publicPaths: ["/", "/api/auth/:path*"],
+//   }
+// );
+
+// export const config = {
+//   matcher: [
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//   ],
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 
 export default withAuth(
-  async function middleware() {
-  },
+  async function middleware() {},
   {
-    // Middleware still runs on all routes, but doesn't protect the blog route
     publicPaths: ["/", "/api/auth/:path*"],
   }
 );
@@ -13,4 +54,4 @@ export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
   ],
-}
+};
